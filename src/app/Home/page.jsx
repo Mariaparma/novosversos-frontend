c316cdbf016047323840306d103b77d8ed11434e
maria parma <mariaparma.tds1.senai@gmail.com>
2025-10-07 16:15:13 -0300

    Update page.jsx

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,88 +1,97 @@
 import React from 'react';
 import styles from './Home.module.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-const HomePage = () => {
+const Home = () => {
   return (
     <div className={`${styles.global} ${styles.homepage}`}>
-      <header className={styles['homepage-header']}>
-        <div className={styles['logo-container']}>
-          <img src="/icons/novosversos.png" alt="Estrela Guia Logo" className={styles.logo} />
-        </div>
-        <nav>
-          <ul className={styles['nav-menu']}>
-            <li><a href="/Noticias">Notícias</a></li>
-            <li><a href="/Shows">Shows</a></li>
-            <li><a href="/Albuns">Álbuns e Músicas</a></li>
-            <li><a href="/Galeria">Galeria</a></li>
-             <li><a href="/Contato">Contato</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main className={styles['homepage-main']}>
+      <Header />
+      <main className={styles.homepagemain}>
         <section className="intro">
-        <h3>Bem-vindo ao Novos Versos</h3>
-        <p>Informações oficiais sobre o cantor Luan Santana</p>
+          <h3>Bem-vindo ao Novos Versos</h3>
+          <p>Informações oficiais sobre o cantor Luan Santana</p>
         </section>
+
         <section className={styles.cards}>
           <div className={styles.card}>
-            <div className={styles['card-image-container']}>
-              <img src="/images/luansantanacapa1.png" alt="Imagem Genérica" className={styles['card-image']} />
+            {/* Card 1 */}
+            <div className={styles.cardimagecontainer}>
+              <img
+                src="/images/luansantanacapa1.png"
+                alt="Imagem Genérica"
+                className={styles.cardimage}
+              />
             </div>
-            <div className={styles['card-content']}>
-              <h4 className={styles['card-title']}>Explore as princípais notícias do Luan Santana</h4>  
-              <h4 className={styles['card-title']}>Descubra os próximos shows do Luan Santana</h4>
-             <a href="/Noticias" className={styles['card-button']}>Descubra Mais</a>
-            </div>
-            
-            <div className={styles['card-image-container']}>
-              <img src="/images/novosversos3.png" alt="Imagem Genérica" className={styles['card-image']} />
-            </div>
-
-            <div className={styles['card-content']}>
-              <h4 className={styles['card-title']}>Descubra os próximos shows do Luan Santana</h4>
-              <h4 className={styles['card-title']}>Descubra os melhores pontos turisticos pra fazer sua viagem mais especial </h4>
-              <a href="/Shows" className={styles['card-button']}>Descubra Mais</a>
+            <div className={styles.cardcontent}>
+              <h4 className={styles.cardtitle}>Explore as principais notícias do Luan Santana</h4>
+              <h4 className={styles.cardtitle}>Descubra os próximos shows do Luan Santana</h4>
+              <a href="/Noticias" className={styles.cardbutton}>Descubra Mais</a>
             </div>
 
-            <div className={styles['card-image-container']}>
-              <img src="/images/novosversos1.png" alt="Imagem Genérica" className={styles['card-image']} />
+            {/* Card 2 */}
+            <div className={styles.cardimagecontainer}>
+              <img
+                src="/images/novosversos3.png"
+                alt="Imagem Genérica"
+                className={styles.cardimage}
+              />
+            </div>
+            <div className={styles.cardcontent}>
+              <h4 className={styles.cardtitle}>Descubra os próximos shows do Luan Santana</h4>
+              <h4 className={styles.cardtitle}>
+                Descubra os melhores pontos turísticos pra fazer sua viagem mais especial
+              </h4>
+              <a href="/Shows" className={styles.cardbutton}>Descubra Mais</a>
             </div>
 
-            <div className={styles['card-content']}>
-              <h4 className={styles['card-title']}>Explore os albúns do Luan Santana </h4>
-              <h4 className={styles['card-title']}>Descubra o trecho das músicas de cada albúm </h4>
-              <a href="/Albuns" className={styles['card-button']}>Descubra Mais</a>
+            {/* Card 3 */}
+            <div className={styles.cardimagecontainer}>
+              <img
+                src="/images/novosversos1.png"
+                alt="Imagem Genérica"
+                className={styles.cardimage}
+              />
+            </div>
+            <div className={styles.cardcontent}>
+              <h4 className={styles.cardtitle}>Explore os álbuns do Luan Santana</h4>
+              <h4 className={styles.cardtitle}>Descubra o trecho das músicas de cada álbum</h4>
+              <a href="/Albuns" className={styles.cardbutton}>Descubra Mais</a>
             </div>
 
-            <div className={styles['card-image-container']}>
-              <img src="/images/novosversos4.png" alt="Imagem Genérica" className={styles['card-image']} />
+            {/* Card 4 */}
+            <div className={styles.cardimagecontainer}>
+              <img
+                src="/images/novosversos4.png"
+                alt="Imagem Genérica"
+                className={styles.cardimage}
+              />
+            </div>
+            <div className={styles.cardcontent}>
+              <h4 className={styles.cardtitle}>Veja a galeria de fotos do cantor</h4>
+              <h4 className={styles.cardtitle}>E participe da área de fãs deixando seu Post!</h4>
+              <a href="/Galeria" className={styles.cardbutton}>Descubra Mais</a>
             </div>
 
-            <div className={styles['card-content']}>
-              <h4 className={styles['card-title']}>Veja a galeria de fotos do cantor  </h4>
-              <h4 className={styles['card-title']}>E participe da área de fãs deixando seu Post! </h4>
-              <a href="/Galeria" className={styles['card-button']}>Descubra Mais</a>
+            {/* Card 5 */}
+            <div className={styles.cardimagecontainer}>
+              <img
+                src="/images/novosversos5.png"
+                alt="Imagem Genérica"
+                className={styles.cardimage}
+              />
             </div>
-
-              <div className={styles['card-image-container']}>
-              <img src="/images/novosversos5.png" alt="Imagem Genérica" className={styles['card-image']} />
+            <div className={styles.cardcontent}>
+              <h4 className={styles.cardtitle}>Entre em contato conosco!</h4>
+              <h4 className={styles.cardtitle}>Tire suas dúvidas</h4>
+              <a href="/Contato" className={styles.cardbutton}>Descubra Mais</a>
             </div>
-
-            <div className={styles['card-content']}>
-              <h4 className={styles['card-title']}>Entre em contato conosco! </h4>
-              <h4 className={styles['card-title']}>Tire suas Dúvidas</h4>
-              <a href="/Contato" className={styles['card-button']}>Descubra Mais</a>
-            </div>
-
-
           </div>
         </section>
       </main>
-      <footer className={styles['homepage-footer']}>
-        <p>&copy; 2025 Estrela Guia. Todos os direitos reservados.</p>
-      </footer>
+      <Footer />
     </div>
   );
 };
 
-export default HomePage;
+export default Home;
