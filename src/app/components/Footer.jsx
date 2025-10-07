505cd391f00d019bd5158ce42500e882422baf5c
maria parma <mariaparma.tds1.senai@gmail.com>
2025-10-07 16:15:05 -0300

    Create Footer.jsx

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css';
+
+export default function Footer () {
+    return (
+        <footer className={styles['homepage-footer']}>
+        <p>&copy; 2025 Estrela Guia. Todos os direitos reservados.</p>
+      </footer>
+    )
+}
